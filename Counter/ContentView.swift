69c3265c14eaed271a3e99af32d8a48262682bcf
Big Guy <themilleraustin@gmail.com>
2025-11-17 19:20:44 -0600

    Initial Commit

---
+++ Counter/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Counter
+//
+//  Created by Big Guy on 11/17/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    
+    var body: some View {
+        VStack {
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
